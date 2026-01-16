d676886c38133e9fc25417bed2a4c99300b34e42
0ctps <6757332+0ctps@users.noreply.github.com>
2026-01-16 17:31:29 +0900

    feat(fish): port zsh configuration to fish shell
    - Add PATH settings for go, cargo, scripts.d and bin directories
    - Add aliases for vim, diff, grep, ls, docker, tmux and grc
    - Add fzf functions (fe, fcd, fh, fkill)
    - Add utility functions (extract, weather, sshmount, sshumount)
    - Add colored man pages and LS_COLORS
    - Add cd function with auto ls
    - Add vi mode jj escape binding
    - Add tmux auto-start on Linux

--- fish/config.fish
+++ fish/config.fish
@@ -309,6 +309,51 @@ end
 # ----------------------------------------------------------------------/
 # [
 
+set -gx LANG en_US.UTF-8
+set -gx LC_ALL en_US.UTF-8
+
+# * »»  path -------------------------------------------/
+# [[
+
+# Go
+if type -q go
+    if test -e $HOME/.go
+        set -gx GOPATH $HOME/.go
+        set -gx PATH $GOPATH/bin $PATH
+        set -gx GO15VENDOREXPERIMENT 1
+    end
+end
+
+# Cargo (Rust)
+if test -e $HOME/.cargo/bin
+    set -gx PATH $HOME/.cargo/bin $PATH
+end
+
+# Scripts
+if test -e $HOME/.scripts.d
+    set -gx PATH $HOME/.scripts.d $PATH
+end
+
+# Local bin
+if test -e $HOME/.bin/bin
+    set -gx PATH $HOME/.bin/bin $PATH
+end
+
+if test -e $HOME/.bin
+    set -gx PATH $HOME/.bin $PATH
+end
+
+if test -e $HOME/bin
+    set -gx PATH $HOME/bin $PATH
+end
+
+if test -e $HOME/usr/bin
+    set -gx PATH $HOME/usr/bin $PATH
+end
+
+# ]]
+# * «« -------------------------------------------------/
+
 # ]
 
 
@@ -320,6 +365,70 @@ end
 # ----------------------------------------------------------------------/
 # [
 
+# vi mode escape with jj
+bind -M insert jj "if commandline -P; commandline -f cancel; else; set fish_bind_mode default; commandline -f backward-char repaint-mode; end"
+
+# ]
+
+
+
+# * »   ALIAS
+# -------------------------------------------                            /
+# ----------------------------------------------------------------------/
+# [
+
+alias v='vim'
+alias diff='diff -u'
+alias grep='grep --color'
+alias ping='ping -n'
+
+# ls
+switch (uname)
+case Darwin
+    alias ls='ls -G -F -L'
+case '*'
+    alias ls='ls -F --color=auto --group-directories-first'
+end
+
+# docker
+alias d='docker'
+alias dc='docker compose'
+alias dcupgrade='docker compose pull && docker compose down && docker compose up -d && yes | docker system prune -a'
+
+# tmux
+if type -q tmux
+    alias tl='tmux ls'
+    alias tk='tmux kill-session -t'
+    alias ta='tmux a'
+    alias tat='tmux a -t'
+end
+
+# pygmentize (ccat)
+if type -q pygmentize
+    alias ccat='pygmentize -O style=monokai -f console256 -g'
+end
+
+# colordiff
+if type -q colordiff
+    alias cdiff='colordiff -u'
+end
+
+# grc (colorize)
+if type -q grc
+    alias mount='grc mount'
+    alias df='grc df'
+    alias ldap='grc ldap'
+    alias dig='grc dig'
+    alias ifconfig='grc ifconfig'
+    alias netstat='grc netstat'
+    # alias ping='grc ping'  # conflicts with ping -n
+    alias ip='grc ip'
+    alias traceroute='grc traceroute'
+    alias ps='grc ps'
+    alias gcc='grc gcc'
+    alias g++='grc g++'
+end
+
 # ]
 
 
@@ -330,6 +439,12 @@ end
 # -------------------------------------------                            /
 # ----------------------------------------------------------------------/
 # [
+
+# LS colors
+set -gx LSCOLORS gxfxcxdxbxegedabagacad
+set -gx LS_COLORS 'no=00;38;5;244:rs=0:di=00;38;5;33:ln=00;38;5;37:mh=00:pi=48;5;230;38;5;136;01:so=48;5;230;38;5;136;01:do=48;5;230;38;5;136;01:bd=48;5;230;38;5;244;01:cd=48;5;230;38;5;244;01:or=48;5;235;38;5;160:su=48;5;160;38;5;230:sg=48;5;136;38;5;230:ca=30;41:tw=48;5;64;38;5;230:ow=48;5;235;38;5;33:st=48;5;33;38;5;230:ex=00;38;5;64:'
+set -gx CLICOLOR true
+set -gx MANPAGER 'less -R'
 
 # ]
 
@@ -358,6 +473,234 @@ case 'MSYS*'
     echo '(X86)'
   end
 
+end
+
+# ]
+
+
+
+# * »   FUNCTION
+# -------------------------------------------                            /
+# ----------------------------------------------------------------------/
+# [
+
+# * »»  fzf functions ----------------------------------/
+# [[
+
+if type -q fzf
+    # fe - edit file with fzf
+    function fe
+        set -l file (fzf --query="$argv[1]" --select-1 --exit-0)
+        if test -n "$file"
+            $EDITOR $file
+        end
+    end
+
+    # fcd - cd to directory with fzf (renamed to avoid conflict with fd command)
+    function fcd
+        set -l dir (find $argv[1] -path '*/\.*' -prune -o -type d -print 2>/dev/null | fzf +m)
+        if test -n "$dir"
+            cd "$dir"
+        end
+    end
+
+    # fh - search history with fzf
+    function fh
+        set -l cmd (history | fzf +s)
+        if test -n "$cmd"
+            commandline -r $cmd
+            commandline -f execute
+        end
+    end
+
+    # fkill - kill process with fzf
+    function fkill
+        set -l signal $argv[1]
+        if test -z "$signal"
+            set signal 9
+        end
+        set -l pid (ps -ef | sed 1d | fzf -m | awk '{print $2}')
+        if test -n "$pid"
+            echo $pid | xargs kill -$signal
+        end
+    end
+end
+
+# ]]
+# * «« -------------------------------------------------/
+
+
+# * »»  extract ----------------------------------------/
+# [[
+
+function extract
+    switch $argv[1]
+        case '*.tar.gz' '*.tgz'
+            tar xzvf $argv[1]
+        case '*.tar.xz'
+            tar Jxvf $argv[1]
+        case '*.zip'
+            unzip $argv[1]
+        case '*.lzh'
+            lha e $argv[1]
+        case '*.tar.bz2' '*.tbz'
+            tar xjvf $argv[1]
+        case '*.tar.Z'
+            tar zxvf $argv[1]
+        case '*.gz'
+            gzip -d $argv[1]
+        case '*.bz2'
+            bzip2 -dc $argv[1]
+        case '*.Z'
+            uncompress $argv[1]
+        case '*.tar'
+            tar xvf $argv[1]
+        case '*.arj'
+            unarj $argv[1]
+        case '*.rar'
+            unrar x $argv[1]
+        case '*'
+            echo "extract: unknown archive type: $argv[1]"
+    end
+end
+
+# ]]
+# * «« -------------------------------------------------/
+
+
+# * »»  cd with ls -------------------------------------/
+# [[
+
+function cd
+    builtin cd $argv
+    and ls
+end
+
+# ]]
+# * «« -------------------------------------------------/
+
+
+# * »»  man (color) ------------------------------------/
+# [[
+
+function man
+    set -lx LESS_TERMCAP_mb (printf "\e[1;31m")
+    set -lx LESS_TERMCAP_md (printf "\e[1;31m")
+    set -lx LESS_TERMCAP_me (printf "\e[0m")
+    set -lx LESS_TERMCAP_se (printf "\e[0m")
+    set -lx LESS_TERMCAP_so (printf "\e[1;44;33m")
+    set -lx LESS_TERMCAP_ue (printf "\e[0m")
+    set -lx LESS_TERMCAP_us (printf "\e[1;32m")
+    command man $argv
+end
+
+# ]]
+# * «« -------------------------------------------------/
+
+
+# * »»  sshfs functions --------------------------------/
+# [[
+
+# sshmount - mount remote directory via sshfs
+function sshmount
+    set -l hostname $argv[1]
+    set -l username $argv[2]
+    set -l server_type $argv[3]
+    set -l mount_dir $argv[4]
+    set -l local_dir $argv[5]
+
+    if test -z "$mount_dir" -o -z "$local_dir"
+        set mount_dir /home/$username
+        set local_dir ~/.mnt/$hostname
+    end
+
+    if test "$server_type" = "local"
+        set destination "$hostname.local"
+    else
+        set destination $hostname
+    end
+
+    if test -d $local_dir
+        set -l mntchk (df | grep -c $local_dir)
+        if test $mntchk -eq 0
+            if ping -c 1 $destination >/dev/null 2>&1; and type -q sshfs
+                sshfs -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no $username@$destination:$mount_dir $local_dir
+            end
+        end
+    end
+end
+
+# sshumount - unmount sshfs mount
+function sshumount
+    set -l hostname $argv[1]
+    set -l username $argv[2]
+    set -l server_type $argv[3]
+    set -l local_dir $argv[4]
+
+    if test -z "$local_dir"
+        set local_dir ~/.mnt/$hostname
+    end
+
+    if test "$server_type" = "local"
+        set destination "$hostname.local"
+    else
+        set destination $hostname
+    end
+
+    if test -d $local_dir
+        set -l mntchk (df | grep -c $local_dir)
+        if test $mntchk -gt 0
+            if ping -c 1 $destination >/dev/null 2>&1
+                umount $local_dir
+            end
+        end
+    end
+end
+
+# ]]
+# * «« -------------------------------------------------/
+
+
+# * »»  weather ----------------------------------------/
+# [[
+
+function weather
+    if type -q curl
+        curl wttr.in/$argv[1]
+    end
+end
+
+# ]]
+# * «« -------------------------------------------------/
+
+# ]
+
+
+
+# * »   STARTUP
+# -------------------------------------------                            /
+# ----------------------------------------------------------------------/
+# [
+
+# tmux auto-start on Linux
+switch (uname)
+case Linux
+    if test -e $HOME/.tmux.conf; and type -q tmux
+        if test -z "$TMUX"; and not status is-login
+            set -l ID (tmux list-sessions 2>/dev/null)
+            if test -z "$ID"
+                tmux new-session
+            else if type -q fzf
+                set -l create_new "Create New Session"
+                set -l choice (printf "%s\n%s:" "$ID" "$create_new" | fzf | cut -d: -f1)
+                if test "$choice" = "$create_new"
+                    tmux new-session
+                else if test -n "$choice"
+                    tmux attach-session -t "$choice"
+                end
+            end
+        end
+    end
 end
 
 # ]
